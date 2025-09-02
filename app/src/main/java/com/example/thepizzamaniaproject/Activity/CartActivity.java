22e461c7eaeffa6d20c522400d8caf8bca2b7270
Durshan2003 <durshan20030102@gmail.com>
2025-09-03 01:33:09 +0530

    Updated activity_cart.xml Updated CartActivity.java created cart_background.xml for cart section.

--- app/src/main/java/com/example/thepizzamaniaproject/Activity/CartActivity.java
+++ app/src/main/java/com/example/thepizzamaniaproject/Activity/CartActivity.java
@@ -1,6 +1,10 @@
 package com.example.thepizzamaniaproject.Activity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,8 +31,88 @@ public class CartActivity extends AppCompatActivity {
 
 
 
-        
+        // Highlight current page
+        highlightCurrentPage();
+
+
+        // Home button
+        findViewById(R.id.homeBtn).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Navigate to Home
+                Intent intent = new Intent(CartActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        // Profile button
+        findViewById(R.id.profileBtn).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(CartActivity.this, ProfileActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        // Cart button
+        findViewById(R.id.cartBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // Navigate to cart
+                highlightCurrentPage();
+            }
+        });
+
+
+        // Settings button
+//        findViewById(R.id.settingsBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Navigate to settings
+//                Intent intent = new Intent(CartActivity.this, SettingsActivity.class);
+//                startActivity(intent);
+//
+//            }
+//        });
+
+
+        // Branches button
+        findViewById(R.id.branchesBtn).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Navigate to branches
+                Intent intent = new Intent(CartActivity.this,BranchesActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
+
+
+    // To highlight the current page text
+    private void highlightCurrentPage()
+    {
+
+        View profileBtn = findViewById(R.id.profileBtn);
+        TextView profileText = profileBtn.findViewById(R.id.profileText);
+
+
+        profileText.setTextColor(Color.parseColor("#FF3D00"));
+
+    }
+
+
+
 }
